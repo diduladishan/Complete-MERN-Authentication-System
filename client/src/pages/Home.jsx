34648200cee13ccf43853e152dbedf231ea317b0
diduladishan <diduladishan14@gmail.com>
2025-02-21 21:14:55 +0530

    feat: Add navigation to login page on button click

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,10 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div>
+    <div
+      className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url('/bg_img.png')` }}
+    >
       <Navbar />
       <Header />
     </div>
