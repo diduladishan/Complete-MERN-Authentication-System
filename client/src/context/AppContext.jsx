6dfdd32729b5d85e70b541f4e77ceb78448fefb1
diduladishan <diduladishan14@gmail.com>
2025-02-22 20:49:43 +0530

    feat: Add AppContext for global state management

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,9 +1,20 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const AppContent = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [userData, setUserData] = useState(false);
+
+  const value = {
+    backendUrl,
+    isLoggedin,
+    setIsLoggedin,
+    userData,
+    setUserData,
+  };
 
   return (
     <AppContent.Provider value={value}>{props.children}</AppContent.Provider>
