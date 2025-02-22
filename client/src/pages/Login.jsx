a5c97147ba052707668e0e73d9c74b54cbec340b
diduladishan <diduladishan14@gmail.com>
2025-02-22 21:30:30 +0530

    feat: add ToastContainer and toast messages to the project

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,16 +2,41 @@ import React, { useContext, useState } from "react";
 import { assets } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import { AppContent } from "../context/AppContext";
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const { backendUrl } = useContext(AppContent);
+  const { backendUrl, setIsLoggedin } = useContext(AppContent);
 
   const [state, setState] = useState("Sign Up");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const onSubitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      axios.defaults.withCredentials = true;
+
+      if (state === "Sign Up") {
+        const { data } = await axios.post(backendUrl + "/api/auth/register", {
+          name,
+          email,
+          password,
+        });
+
+        if (data.success) {
+          setIsLoggedin(true);
+          navigate("/");
+        } else {
+          alert(data.message);
+        }
+      } else {
+      }
+    } catch (error) {}
+  };
 
   return (
     <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400">
@@ -33,7 +58,7 @@ const Login = () => {
             : "Login to your account!"}
         </p>
 
-        <form>
+        <form onSubmit={onSubitHandler}>
           {state === "Sign Up" && (
             <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5c]">
               <img src={assets.person_icon} alt="" />
